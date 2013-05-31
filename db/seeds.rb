4cef7622d1bf5c869bbacdd326ce0e601cdd5fe9
Bill Davis <bdavis84@pacbell.net>
2013-05-31 00:30:01 -0700

    added unverified and banned test users to the db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup, :confirmed_at => DateTime.now, :pilot_name => "Test_Pilot", :role => "admin"
 puts 'user: ' << user.email
+
+user2 = User.find_or_create_by_email :email => "unverified@test.com", :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup, :confirmed_at => DateTime.now, :pilot_name => "Test_Unverified_Pilot", :role => "unverified"
+puts 'user: ' << user2.email
+
+user3 = User.find_or_create_by_email :email => "banned@test.com", :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup, :confirmed_at => DateTime.now, :pilot_name => "Test_Banned_Pilot", :role => "banned"
+puts 'user: ' << user3.email
