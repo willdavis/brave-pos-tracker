20daa805d503b312ea263c18a991d282c1e50d42
Bill Davis <bdavis84@pacbell.net>
2013-05-30 21:59:38 -0700

    added admin roles to the ability class.  Also added Roles pulldown menu in User#edit view

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :manage, :all if user.role == "admin"
+    
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
