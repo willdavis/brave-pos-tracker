bb7bc4665664c175bf9b09fd4b5a28004c63a984
Bill Davis <bdavis84@pacbell.net>
2013-05-30 21:00:06 -0700

    update seed and user model to allow for pilot_name mass assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130531003451) do
+ActiveRecord::Schema.define(:version => 20130531034728) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -30,10 +30,12 @@ ActiveRecord::Schema.define(:version => 20130531003451) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "pilot_name",             :default => "", :null => false
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["pilot_name"], :name => "index_users_on_pilot_name", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
