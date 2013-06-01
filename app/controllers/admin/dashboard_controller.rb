0d78e8fd26c54e2bd441d5813310f09b75ad3d92
Bill Davis <bdavis84@pacbell.net>
2013-05-31 20:14:29 -0700

    added list of unverified users to admin application

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate_user!
   
   def applications
+    @users = User.where("role = 'unverified' AND confirmed_at NOT NULL")
   end
 end
