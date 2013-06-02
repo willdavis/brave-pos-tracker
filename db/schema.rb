d21ba5bb3a2e1fbdb0290981264e31ec1169e0a9
Bill Davis <bdavis84@pacbell.net>
2013-06-02 00:46:20 -0700

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130601075611) do
+ActiveRecord::Schema.define(:version => 20130602074045) do
 
   create_table "admin_roles", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(:version => 20130601075611) do
   create_table "roles_users", :id => false, :force => true do |t|
     t.integer "role_id"
     t.integer "user_id"
+  end
+
+  create_table "stars", :force => true do |t|
+    t.string   "name"
+    t.decimal  "security"
+    t.integer  "gate_count"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
