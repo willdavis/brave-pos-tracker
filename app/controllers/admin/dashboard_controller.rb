bc6fd38517067ac1e34f7f7c44f5800453b5294c
Bill Davis <bdavis84@pacbell.net>
2013-06-01 21:40:10 -0700

    applications view now displays unverified users

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate_user!
   
   def applications
-    @users = User.where("confirmed_at NOT NULL")
+    @users = User.joins('LEFT OUTER JOIN roles_users ON roles_users.user_id = users.id WHERE users.confirmed_at NOT NULL AND roles_users.role_id IS NULL')
     authorize! :manage, @users
   end
 end
