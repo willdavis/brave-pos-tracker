1b4a941b28e3a94a09b01c41237f8ee82ac0878d
Bill Davis <bdavis84@pacbell.net>
2013-06-02 17:41:33 -0700

    changed initialize_on_precompile to false

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,9 @@ module BravePosTracker
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
+    
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
 
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
