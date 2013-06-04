ac79e72a287afd7dcd58352ba3e13ffec5d5fbb2
Bill Davis <bdavis84@pacbell.net>
2013-06-03 18:18:23 -0700

    added authorization to Scouting::Report model

--- app/controllers/scouting/reports_controller.rb
+++ app/controllers/scouting/reports_controller.rb
@@ -1,4 +1,7 @@
 class Scouting::ReportsController < ApplicationController
+  before_filter :authenticate_user!
+  load_and_authorize_resource
+  
   # GET /scouting/reports
   # GET /scouting/reports.json
   def index
