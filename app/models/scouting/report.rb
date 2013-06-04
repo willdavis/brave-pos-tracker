e56f7b70b477ae50892936864df6a8bbe052b569
Bill Davis <bdavis84@pacbell.net>
2013-06-03 17:39:15 -0700

    added belongs_to :user to Report model

--- app/models/scouting/report.rb
+++ app/models/scouting/report.rb
@@ -1,6 +1,8 @@
 class Scouting::Report < ActiveRecord::Base
   attr_accessible :planet_ids, :star_id
   
+  belongs_to :user
+  
   validates :star_id, :presence => true
   
   def star
--- spec/models/scouting/report_spec.rb
+++ spec/models/scouting/report_spec.rb
@@ -4,6 +4,15 @@ describe Scouting::Report do
   before(:each) do
     @report = FactoryGirl.create(:scouting_report)
   end
+  
+  describe ".user" do
+    it "returns a User object" do
+      user = FactoryGirl.create(:user)
+      @report.user_id = user.id
+      
+      @report.user.should eq(user)
+    end
+  end
 
   describe ".star" do
     it "returns a Star object" do
