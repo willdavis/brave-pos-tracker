8307543361a06cf57848fe1fc650db838fabba3a
Bill Davis <bdavis84@pacbell.net>
2013-06-04 15:40:24 -0700

    created migration to add ice_belt_count to planets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130604052608) do
+ActiveRecord::Schema.define(:version => 20130604223153) do
 
   create_table "admin_roles", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20130604052608) do
     t.datetime "created_at",                          :null => false
     t.datetime "updated_at",                          :null => false
     t.integer  "star_id"
+    t.integer  "ice_belt_count",      :default => 0,  :null => false
   end
 
   add_index "planets", ["name"], :name => "index_planets_on_name", :unique => true
