f557ab87967fc2f4926b68b0d274afdb9c2f6c4b
Bill Davis <bdavis84@pacbell.net>
2013-06-08 21:08:27 -0700

    added HABTM relation to Reports and Control Towers

--- app/models/scouting/control_tower.rb
+++ app/models/scouting/control_tower.rb
@@ -1,3 +1,5 @@
 class Scouting::ControlTower < ActiveRecord::Base
   attr_accessible :constellation_id, :control_tower_type_id, :moon_id, :region_id, :solar_system_id
+  
+    has_and_belongs_to_many :reports, :class_name => "Scouting::Report"
 end
--- app/models/scouting/report.rb
+++ app/models/scouting/report.rb
@@ -2,6 +2,7 @@ class Scouting::Report < ActiveRecord::Base
   attr_accessible :star_id
   
   belongs_to :user
+  has_and_belongs_to_many :control_towers, :class_name => "Scouting::ControlTower"
   
   validates :star_id, :presence => true
 end
