a7bb1969959928710cbfca35ce7b150883d2793d
Bill Davis <bdavis84@pacbell.net>
2013-06-11 21:27:34 -0700

    added dscan and probe text areas

--- app/models/scouting/report.rb
+++ app/models/scouting/report.rb
@@ -1,5 +1,6 @@
 class Scouting::Report < ActiveRecord::Base
   attr_accessible :solar_system_id, :solar_system_name
+  attr_accessor :raw_dscan_data, :raw_probe_data
   
   belongs_to :user
   has_and_belongs_to_many :control_towers, :class_name => "Scouting::ControlTower"
