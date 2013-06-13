26a6b2979499e19d4a78c50ee70b62616181e5c8
Bill Davis <bdavis84@pacbell.net>
2013-06-13 16:03:40 -0700

    added control tower and moon names to attr_accessible

--- app/models/scouting/control_tower.rb
+++ app/models/scouting/control_tower.rb
@@ -1,11 +1,6 @@
 class Scouting::ControlTower < ActiveRecord::Base
-  attr_accessible :constellation_id, :control_tower_type_id, :moon_id, :region_id, :solar_system_id
+  attr_accessible :control_tower_type_id,  :control_tower_type_name, :moon_id, :moon_name, 
+    :region_id, :solar_system_id, :constellation_id
   
     has_and_belongs_to_many :reports, :class_name => "Scouting::Report"
-    
-    validates :region_id,             :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }
-    validates :constellation_id,      :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }
-    validates :solar_system_id,       :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }
-    validates :moon_id,               :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }
-    validates :control_tower_type_id, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }
 end
