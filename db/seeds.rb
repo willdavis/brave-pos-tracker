9896b5e4d1c0371bd155ca258ca3337a0b25be5c
Bill Davis <bdavis84@pacbell.net>
2013-06-15 23:16:23 -0700

    updated db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,13 +17,6 @@ role3 = Admin::Role.find_or_create_by_name :name => 'banned'
 puts 'role: ' << role3.name
 
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup, :confirmed_at => DateTime.now, :pilot_name => "Test_Pilot"
+user = User.find_or_create_by_email :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup, :confirmed_at => DateTime.now, :pilot_name => ENV['ADMIN_PILOT_NAME'].dup
 user.roles << role
 puts 'user: ' << user.email
-
-user2 = User.find_or_create_by_email :email => "unverified@test.com", :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup, :confirmed_at => DateTime.now, :pilot_name => "Test_Unverified_Pilot"
-puts 'user: ' << user2.email
-
-user3 = User.find_or_create_by_email :email => "banned@test.com", :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup, :confirmed_at => DateTime.now, :pilot_name => "Test_Banned_Pilot"
-user3.roles << role3
-puts 'user: ' << user3.email
