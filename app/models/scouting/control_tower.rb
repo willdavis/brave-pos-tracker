6fa34b8ab0a3c3314e39202ebea660c7d2714e1f
Bill Davis <bdavis84@pacbell.net>
2013-06-20 16:56:37 -0700

    added structure_ids to attr_accessible

--- app/models/scouting/control_tower.rb
+++ app/models/scouting/control_tower.rb
@@ -1,6 +1,6 @@
 class Scouting::ControlTower < ActiveRecord::Base
   attr_accessible :control_tower_type_id,  :control_tower_type_name, :moon_id, :moon_name, 
-    :region_id, :solar_system_id, :constellation_id, :corporation_id
+    :region_id, :solar_system_id, :constellation_id, :corporation_id, :structure_ids
   attr_accessor :distance
   
     has_and_belongs_to_many :reports, :class_name => "Scouting::Report"
