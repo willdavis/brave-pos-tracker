fcc01027ea7b6233c11acb23722b5b6646288385
Bill Davis <bdavis84@pacbell.net>
2013-06-21 20:17:30 -0700

    added initializer for redis

---
+++ config/initializers/redis.rb
@@ -0,0 +1,8 @@
+require "redis"
+
+if ::Rails.env == "production"
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  REDIS = Redis.new
+end
