76dee893a6302a62d6d14d633949c4af9f3c364f
Bill Davis <bdavis84@pacbell.net>
2013-06-23 21:59:09 -0700

    updated dev initializer to point to local redis server

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,4 @@
 require "redis"
 
-if ::Rails.env == "production"
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-  REDIS = Redis.new
-end
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
