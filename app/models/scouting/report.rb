fe33be02f0c339ef3a7aa7e21930624ef569134d
Bill Davis <bdavis84@pacbell.net>
2013-06-25 14:54:01 -0700

    job_id can now be mass assigned

--- app/models/scouting/report.rb
+++ app/models/scouting/report.rb
@@ -1,5 +1,5 @@
 class Scouting::Report < ActiveRecord::Base
-  attr_accessible :solar_system_id, :solar_system_name, :user_id, :raw_dscan_data, :raw_probe_data
+  attr_accessible :solar_system_id, :solar_system_name, :user_id, :raw_dscan_data, :raw_probe_data, :job_id
   attr_accessor :raw_dscan_data, :raw_probe_data
   
   belongs_to :user
