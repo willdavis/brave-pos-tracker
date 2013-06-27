fe7f783924bf57d646854ff6e7b0c82097f9aa7a
Bill Davis <bdavis84@pacbell.net>
2013-06-26 20:30:49 -0700

    spawned sidekiq with unicorn and removed from Procfile

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,14 @@
 timeout 15
 preload_app true
-worker_processes 2
+worker_processes 3
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
+  
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
@@ -15,6 +17,13 @@ end
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+  
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
   end
 
   defined?(ActiveRecord::Base) and
