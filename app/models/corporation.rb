4db9a0ad9e11db76cef14f37c4c7d79d3714e69c
Bill Davis <bdavis84@pacbell.net>
2013-07-02 15:08:20 -0700

    added checks for nil values

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -13,42 +13,47 @@ class Corporation
   attr_accessor *ATTRIBUTES
   
   def initialize(corp_id)
-    corp = Nokogiri::XML(open("https://api.eveonline.com/corp/CorporationSheet.xml.aspx?corporationID=#{corp_id}"))
+    begin
+      corp = Nokogiri::XML(open("https://api.eveonline.com/corp/CorporationSheet.xml.aspx?corporationID=#{corp_id}"))
     
-    ATTRIBUTES.each do |attribute|
-      send("#{attribute}=", corp.xpath("//result/#{attribute}"))
+      ATTRIBUTES.each do |attribute|
+        send("#{attribute}=", corp.xpath("//result/#{attribute}"))
+      end
+    rescue OpenURI::HTTPError => error
+      puts "Invalid corporation id.  Unable to parse eve api response.\nError:#{error}"
+      false
     end
   end
   
   def id
-    return corporationID.text.to_i
+    corporationID.nil? ? -1 : corporationID.text.to_i
   end
   
   def ceo_id
-    return ceoID.text.to_i
+    ceoID.nil? ? -1 : ceoID.text.to_i
   end
   
   def alliance_id
-    return allianceID.text.to_i
+    allianceID.nil? ? -1 : allianceID.text.to_i
   end
   
   def name
-    return  corporationName.text
+    corporationName.nil? ? nil : corporationName.text
   end
   
   def ticker_symbol
-    return ticker.text
+    ticker.nil? ? nil : ticker.text
   end
   
   def ceo_name
-    return ceoName.text
+    ceoName.nil? ? nil : ceoName.text
   end
   
   def alliance_name
-    return allianceName.text
+    allianceName.nil? ? nil : allianceName.text
   end
   
   def member_count
-    return memberCount.text.to_i
+    memberCount.nil? ? nil : memberCount.text.to_i
   end
 end
