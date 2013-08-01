a506239c2df91ab502bba15107b712ffd71fc182
Bill Davis <bdavis84@pacbell.net>
2013-07-31 23:06:21 -0700

    generated new rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BravePosTracker::Application.config.secret_token = '31bb6f41e95cb9e8dd4b8c769ee6779bdf5311fa346413c2bff025a797c655e48eec2fe11bb62924edc9067ebd56c5ecfc48eb81acf02ca6a8532789bdd2fc96'
