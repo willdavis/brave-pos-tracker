1cf824e1654e57a99a363eeb36d61570449cdf71
Bill Davis <bdavis84@pacbell.net>
2013-08-02 21:05:37 -0700

    corporation has many control towers

--- app/models/control_tower.rb
+++ app/models/control_tower.rb
@@ -1,3 +1,5 @@
 class ControlTower < ActiveRecord::Base
   attr_accessible :constellation_id, :corporation_id, :moon_id, :moon_name, :region_id, :solar_system_id, :type_id
+  
+  belongs_to :corporation
 end
--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -1,3 +1,5 @@
 class Corporation < ActiveRecord::Base
   attr_accessible :eve_id, :name
+  
+  has_many :control_towers
 end
