fe00ebdc2449d5f14519ddd783775bc2891b213f
Bill Davis <bdavis84@pacbell.net>
2013-08-04 01:44:34 -0700

    added validations to corporation model

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -1,5 +1,7 @@
 class Corporation < ActiveRecord::Base
   attr_accessible :eve_id, :name
-  
   has_many :control_towers
+  
+  validates :name,    :presence => true
+  validates :eve_id,  :presence => true
 end
